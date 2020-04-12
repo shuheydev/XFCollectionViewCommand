62c64d301bd6ff4e0f2004988a05b5e898b57572
Shuhey <shuhey.dev@gmail.com>
2020-04-12 16:38:13 +0900

    Add project files.

---
+++ XFCollectionViewCommand/XFCollectionViewCommand/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFCollectionViewCommand
+{
+    class MainPageViewModel
+    {
+        public List<string> People { get; set; }
+        public MainPageViewModel()
+        {
+            People = new List<string>();
+            People.AddRange(new[]
+            {
+                "person1",
+                "person2",
+                "person3",
+                "person4",
+            });
+
+            TappedCommand = new Command(OnTapped);
+        }
+
+        private void OnTapped(object obj)
+        {
+            //タップされたときの処理
+        }
+
+        public Command TappedCommand { get; }
+    }
+}
